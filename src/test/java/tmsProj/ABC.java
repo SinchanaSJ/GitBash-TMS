746ee3a86dfb90beb6b676f570328c1f8b5f4784
SinchanaSJ <sinchanasinchhu@gmail.com>
2023-09-28 15:41:26 +0530

    ABCS Commit

---
+++ src/test/java/tmsProj/ABC.java
@@ -0,0 +1,5 @@
+package tmsProj;
+
+public class ABC {
+
+}
---
+++ src/test/java/tmsProj/ABC2.java
@@ -0,0 +1,10 @@
+package tmsProj;
+
+import org.testng.annotations.Test;
+
+public class ABC2 {
+	@Test
+	public void abc() {
+		System.out.println("Abc");
+	}
+}
